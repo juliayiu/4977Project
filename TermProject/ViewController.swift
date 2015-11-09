a187b43d0bcdc72af34adb20756620a33c26d55b
Evanna Wong <a00807066@dhcp-142-232-233-197.wifi.bcit.ca>
2015-11-09 14:13:00 -0800

    Initial Commit

---
+++ TermProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TermProject
+//
+//  Created by Evanna Wong on 2015-11-09.
+//  Copyright © 2015 Evanna Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
